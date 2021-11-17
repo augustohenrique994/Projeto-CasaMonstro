56938143ac6aa5636c5b5ee386f81e4e2b6a80f9
augustohenrique994 <augusto.silva@bandtec.com.br>
2021-11-17 11:37:15 -0300

    adição dos arquivos para o git

---
+++ Projeto Individual/Banco de dados/Script projeto individual.sql
@@ -0,0 +1,12 @@
+create database Projeto;
+use projeto;
+create table Cadastro (
+EmailCadastro varchar(65),
+NomeUsuario varchar(45),
+SenhaCadastro varchar(45),
+ConfirmarSenha varchar(45)
+);
+create table login (
+NomeUsuario varchar(45),
+Senhausuario varchar(45),
+emailUsuario varchar(65));
